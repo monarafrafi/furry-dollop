aa8093e6595c3023567337223a2cd6e5fa111d08
monarafrafi <mona.rafrafi@gmail.com>
2018-09-17 15:32:41 +0200

    Replace InvoiceLine and Product by dataclasses. Add some preperties to calculate the amount automatically

--- factures.py
+++ factures.py
@@ -1,66 +1,72 @@
-import itertools
+from dataclasses import dataclass
+
+
+@dataclass
 class Product:
     """
     This class describes a given Product to buy
     """
+    name: str
+    price: float
 
-    def __init__(self, name, price):
-        self.price = price
-        self.name = name
+@dataclass
+class InvoiceLine:
+    """
+    This class describes a given line of an Invoice
+    """
 
+    product: Product
+    quantity: int = 1
 
-class FactureLine:
-
-    def __init__(self, product, quantity=1):
+    @property
+    def amount(self):
         """
-        :param product: ex : android phone!, of Product class
-        :param price: in euros
-        :param quantity: the items quantity
+        :return: the price of one invoice line in euros
         """
-        self.product = product
-        self.quantity = quantity
-        self.line_price = product.price*quantity
+        return self.product.price*self.quantity
 
     def __str__(self):
-        return "This is a facture lines"
+        return "This is a invoice line"
 
 
-class Facture:
+class Invoice:
 
-    def __init__(self, name, facture_lines, tva = 1.2):
+    # This is a unique number describing each invoice instance
+    invoice_number = 0
+
+    def __init__(self, client, invoice_lines, tva=1.2):
         """
         :param name: ex : Mona
-        :param facture_lines: list of facture lines
-        :param global_amount:
+        :param invoice_lines: a list of invoice lines
         :param tva:
         """
-        self.name = name
-        self.facture_lines = facture_lines
-        self.global_price = 0
-        for i, facture_line in enumerate(self.facture_lines):
-            self.global_price += facture_line.line_price
+        self.client = client
+        self.invoice_lines = invoice_lines
+        self.tva = tva
+        Invoice.invoice_number += 1
 
-        self.global_price = self.global_price*tva
+    @property
+    def final_price(self):
+        """
+        This property calculates the final price of the invoice, TVA included
+        :return: the final price
+        """
+        return sum(invoice_line.amount for invoice_line in self.invoice_lines)*self.tva
 
     def __str__(self):
 
-        my_facture_description = "Nom :" + self.name + "\n"
-        for facture_line in self.facture_lines:
-            my_facture_description += "\n" + facture_line.product.name + " quantity : "+ str(facture_line.quantity) \
-                                      + " price : "+ str(facture_line.line_price)
-        return my_facture_description
+        my_invoice_description = [f"Number : {Invoice.invoice_number} Client: {self.client}"]
+        for invoice_line in self.invoice_lines:
+            my_invoice_description.append(f"{invoice_line.product.name} quantity : {invoice_line.quantity} price : {invoice_line.amount}")
+        return '\n'.join(my_invoice_description)
 
 
 
-
-
-def build_factures():
+def build_invoice():
     """
-    This function creates a facture with wonderful items
+    This function creates a invoice with wonderful items
     :return:
     """
-
-    # TODO : ceci est moche, utiliser pytest!
 
     # Mona achète 3 animaux en plastique
     dragon = Product("red_dragon", 10)
@@ -68,34 +74,34 @@ def build_factures():
     cat = Product("lolcat", 5)
 
     # Elle en veut plusieurs de chaque pour faire une ferme magique!
-    facture_lines = []
-    # un seul dragon sinon ils s'entretuent
-    facture_lines.append(FactureLine(dragon, 1))
-    facture_lines.append(FactureLine(unicorn, 10))
-    facture_lines.append(FactureLine(cat, 42))
+    invoice_lines = [InvoiceLine(dragon, 1), InvoiceLine(unicorn, 10), InvoiceLine(cat, 42)]
 
-    global_facture = Facture("Mona", facture_lines)
-    print("Voici le cout de la ferme magique : ", global_facture.global_price, "euros seulement!")
-    print(global_facture)
-    return global_facture
+    invoice = Invoice("Mona", invoice_lines)
+    print("Voici le cout de la ferme magique : ", invoice.final_price, "euros seulement!")
+    print(invoice)
+    return invoice
 
-def test_factures(facture):
+
+def test_invoice():
     """
-    We test that a given facture is correctly built
+    We test that a given invoice is correctly built
     :return:
     """
-    # We check that the facture lines are correct. We also check the total amount
+    # We create an incoive
+    invoice = build_invoice()
+    # We check that the invoice lines are correct. We also check the total amount
     products_names = ["red_dragon", "cute_unicorn", "lolcat"]
 
-    for facture_line, products_name in zip(facture.facture_lines, products_names):
-        assert facture_line.product.name == products_name
-    assert facture.global_price == 504.0
+    for invoice_line, products_name in zip(invoice.invoice_lines, products_names):
+        assert invoice_line.product.name == products_name
+    assert invoice.final_price == 504.0
+
+    # We change the TVA, check that the total amount is correctly calculated
+    invoice.tva = 1.3
+    assert invoice.final_price == 546.0
 
 
 if __name__ == "__main__":
-    print("enter main")
-    my_facture = build_factures()
-    test_factures(my_facture)
-    # ugly, to be replaced by pytest
-    # Tester le montant global, valide quantité, prix, etc...
+    test_invoice()
+
 

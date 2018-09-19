80cb67f0993959519625c32b013ec96a7fd7d632
monarafrafi <mona.rafrafi@gmail.com>
2018-09-19 11:41:03 +0200

    Add responses in requirements

--- test_mock_example.py
+++ test_mock_example.py
@@ -1,14 +1,40 @@
+import responses
 from mock_example import get_user, HttpNotFound, User, APIUnreachableException
 import unittest
+
+MOCK_USER = {
+    'results': [
+        {
+            'gender': 'female',
+            'name': {
+                'title': 'mrs',
+                'first': 'mona',
+                'last': 'rafrafi'
+            },
+
+            'email': 'mona.rafrafi@example.com',
+            'login': {
+                'username': 'greenladybug656'
+            },
+        }
+]
+}
 
 
 class GetUserTestCase(unittest.TestCase):
 
-    def test_get_user_works(self):
+    @responses.activate
+    def test_get_user_mock_server(self):
+        responses.add(responses.GET, 'https://randomuser.me/api', json=MOCK_USER)
         user = get_user()
         self.assertIsInstance(user, User)
+        self.assertEqual(user.firstname,'mona')
+        self.assertEqual(user.lastname, 'rafrafi')
+        self.assertEqual(user.gender, 'female')
+        self.assertEqual(user.email, 'mona.rafrafi@example.com')
+        self.assertEqual(user.username, 'greenladybug656')
 
-    @unittest.skip('not implemented')
+    @unittest.skip('in progress')
     def test_get_user_not_found(self):
         with self.assertRaises(HttpNotFound):
             user = get_user()

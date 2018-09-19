c16f8008c71f4b942a01928697695cdfff196f1c
monarafrafi <mona.rafrafi@gmail.com>
2018-09-19 14:58:50 +0200

    Add mock for get_user

--- test_mock_example.py
+++ test_mock_example.py
@@ -1,5 +1,8 @@
+from unittest import mock
+
 import responses
-from mock_example import get_user, HttpNotFound, User, APIUnreachableException
+# Attention, si on importe la fonction
+import mock_example # import get_user, HttpNotFound, User, APIUnreachableException
 import unittest
 
 MOCK_USER = {
@@ -21,13 +24,13 @@ MOCK_USER = {
 }
 
 
-class GetUserTestCase(unittest.TestCase):
+class GetUserInternalTestCase(unittest.TestCase):
 
     @responses.activate
     def test_get_user_mock_server(self):
         responses.add(responses.GET, 'https://randomuser.me/api', json=MOCK_USER)
-        user = get_user()
-        self.assertIsInstance(user, User)
+        user = mock_example.get_user()
+        self.assertIsInstance(user, mock_example.User)
         self.assertEqual(user.firstname,'mona')
         self.assertEqual(user.lastname, 'rafrafi')
         self.assertEqual(user.gender, 'female')
@@ -38,14 +41,24 @@ class GetUserTestCase(unittest.TestCase):
     @responses.activate
     def test_get_user_not_found(self):
         responses.add(responses.GET, 'https://randomuser.me/api', status=404)
-        with self.assertRaises(HttpNotFound):
-            user = get_user()
+        with self.assertRaises(mock_example.HttpNotFound):
+            user = mock_example.get_user()
 
     @responses.activate
     def test_get_user_server_is_unreachable(self):
         # By default, with responses, we have a connexion error by default
-        with self.assertRaises(APIUnreachableException):
-            user = get_user()
+        with self.assertRaises(mock_example.APIUnreachableException):
+            user = mock_example.get_user()
+
+
+class GetUserTestCase(unittest.TestCase):
+
+    @mock.patch('mock_example.get_user')
+    def test(self, my_mock_test_user):
+        from mock_example import get_user, User
+        my_mock_test_user.return_value = User.create_from_api(MOCK_USER)
+        user = get_user()
+        self.assertEqual(user.firstname, 'mona')
 
 
 

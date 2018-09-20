4eb940ab8a1bd9e80099a55e1fb261e6cd9d24be
monarafrafi <mona.rafrafi@gmail.com>
2018-09-20 10:54:16 +0200

    Example of optimisation with lists

--- test_memory_profiler.py
+++ test_memory_profiler.py
@@ -1,20 +1,21 @@
 # from memory_profiler import profile
 import os
+import cProfile
 
-@profile
 def my_func():
     a = [1] * (10**6)
     b = [2] * (2*10**7)
     del b
     return a
 
-@profile
+# slow 0.5
 def function():
     lst = []
     for i in range(1000000):
         lst.append(i)
     return lst
 
+# a liiittle better 0.5
 def function_with_shortcut():
     lst = []
     append = lst.append
@@ -22,13 +23,17 @@ def function_with_shortcut():
         append(i)
     return lst
 
+# much better! 0.07
 def function_list_comprehension():
     return [i for i in range(1_000_000)]
 
+# even better ! 0.01
 def function_convert_to_list():
     return list(range(1_000_000))
 
 
 if __name__ == '__main__':
-    for i in range(10):
-        my_func()
+    function()
+    function_with_shortcut()
+    function_list_comprehension()
+    function_convert_to_list()
